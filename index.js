1aba254da4c3500eda6789c709d88122f75a14cb
AcevedoBot <ogalbeiro@gmail.com>
2024-06-02 17:05:05 -0400

    index.js

--- index.js
+++ index.js
@@ -36,9 +36,8 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Analizando tiempos de vueltas
-", "Ajustando configuraciones del coche
-", "Viendo la carrera en vivo"];
+const statusMessages = ["Analizando tiempos de vueltas","Ajustando configuraciones del coche
+","Viendo la carrera en vivo"];
 
 
 let currentIndex = 0;
